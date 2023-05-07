0f0816a92b4904702646a33dcb59e281950c44b9
unvalley <kirohi.code@gmail.com>
2023-05-07 14:33:31 +0900

    chore: rename dir

--- clients/desktop/src/main.tsx
+++ clients/desktop/src/main.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react"
-import { extendTheme } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
-  colors: {
-    transparent: "transparetn",
-    black: "#000",
-    white: "#fff",
-  },
+	colors: {
+		transparent: "transparetn",
+		black: "#000",
+		white: "#fff",
+	},
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ChakraProvider theme={theme}>
+			<App />
+		</ChakraProvider>
+	</React.StrictMode>,
 );
